eb25c6cbc8be4eaecaa58422993ced3499cc0e08
Omkar Singh <frnk.kiyo007@gmail.com>
2026-02-14 15:41:03 +0530

    Fixed error printing message to stderr. Fixed all the edge cases. Passed all test cases of Ostep.

--- reverse.c
+++ reverse.c
@@ -10,11 +10,11 @@ struct node{
 
 int main(int argc, char* argv[]) {
     if(argc>3) {
-        printf("Usage: ./reverse <input.txt> <output.txt>");
+        fprintf(stderr, "usage: reverse <input> <output>\n");
         exit(1);
     }
     if(argc == 3 && strcmp(argv[1], argv[2])==0) {
-        printf("Input and Output file must be different\n");
+        fprintf(stderr, "reverse: input and output file must differ\n");
         exit(1);
     }  
     struct node *head = NULL;
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]) {
     if(argc>1) {
         input_stream = fopen(argv[1], "r");
         if(!input_stream) {
-            fprintf(stderr, "Error opening input file\n");
+            fprintf(stderr, "reverse: cannot open file \'%s\'\n", argv[1]);
             exit(1);
         }
     }
@@ -32,14 +32,14 @@ int main(int argc, char* argv[]) {
     if(argc>2) {
         output_stream = fopen(argv[2], "w");
         if(!output_stream) {
-            fprintf(stderr, "Error opening output file\n");
+            fprintf(stderr, "reverse: cannot open file \'%s\'\n", argv[2]);
             exit(1);
         }
     }
     while(getline(&c, &cap, input_stream) != -1) {
         struct node *newnode = (struct node*)malloc(sizeof(struct node));
         if(newnode == NULL) {
-            fprintf(stderr, "Memory overflow");
+            fprintf(stderr, "Memory overflow\n");
             exit(1);
         }
         newnode->s = strdup(c);
@@ -51,6 +51,10 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
     free(c);
+    if(head == NULL) {
+        fprintf(stderr, "reverse: input and output file must differ\n");
+        exit(1);
+    }
     while(head != NULL) {
         struct node *tmp = head;
         fputs(head->s, output_stream);
